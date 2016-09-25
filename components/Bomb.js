68c4882c3f442fc094a54996753012cf9f9ab7dd
Troy Chryssos <github email address>
2016-09-25 17:49:25 -0400

    Used initial states to pass dynamic text through to JXS renders

---
+++ components/Bomb.js
@@ -0,0 +1,22 @@
+const React=require('react')
+
+class Bomb extends React.Component{
+  constructor(props){
+    super()
+    this.state={secondsLeft:props.initialCount}
+  }
+
+  render(){
+    if (this.state.secondsLeft===0){
+      return(
+        <p>Boom!</p>
+      )
+    } else {
+      return (
+        <p>{this.state.secondsLeft} seconds left before I go boom!</p>
+      )
+    }
+  }
+}
+
+module.exports=Bomb
